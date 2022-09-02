dcf6ea487113a558d7c1a23092725a1a94135835
juan.tabares <juan.tabares@coytex.com.co>
2022-09-02 16:34:22 -0500

    Personalizando vista verify-email

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -7,12 +7,12 @@
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
-            {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+            {{ __('Es necesario confirmar tu cuenta antes de continuar, revisa tu email y presiona sobre el enlace de confirmación.') }}
         </div>
 
         @if (session('status') == 'verification-link-sent')
             <div class="mb-4 font-medium text-sm text-green-600">
-                {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+                {{ __('Hemos enviado un nuevo email de confirmación a la cuenta que colocaste en el registro.') }}
             </div>
         @endif
 
@@ -22,7 +22,7 @@
 
                 <div>
                     <x-button>
-                        {{ __('Resend Verification Email') }}
+                        {{ __('Enviar Email de confirmación') }}
                     </x-button>
                 </div>
             </form>
@@ -30,8 +30,8 @@
             <form method="POST" action="{{ route('logout') }}">
                 @csrf
 
-                <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    {{ __('Log Out') }}
+                <button type="submit" class="text-sm text-gray-600 hover:text-gray-900">
+                    {{ __('Cerrar sesión') }}
                 </button>
             </form>
         </div>
