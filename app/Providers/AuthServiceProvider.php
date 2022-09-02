db190ef53a6bcd523526ec34f5b5e2f434685057
juan.tabares <juan.tabares@coytex.com.co>
2022-09-02 16:41:01 -0500

    modificando email de verificacion

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +28,12 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        VerifyEmail::toMailUsing(function($notifiable, $url) {
+            return (new MailMessage)
+                ->subject('Verificar Cuenta')
+                ->line('Tu Cuenta ya está casi lista, solo debes presionar el enlace a continuación')
+                ->action('Confirmar Cuenta', $url)
+                ->line('Si no creaste esta cuenta, puedes ignorar este mensaje.');
+        });
     }
 }
