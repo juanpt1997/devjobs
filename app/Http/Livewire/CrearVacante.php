2840f4b3022c5ffd51ae9ea6613bbd860b21352a
juan.tabares <juan.tabares@coytex.com.co>
2022-09-03 18:39:24 -0500

    Creado modelo Salario y agregado a la vista el select de estos salarios

--- app/Http/Livewire/CrearVacante.php
+++ app/Http/Livewire/CrearVacante.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Salario;
 use Livewire\Component;
 
 class CrearVacante extends Component
 {
     public function render()
     {
-        return view('livewire.crear-vacante');
+        // Consultar BD
+        $salarios = Salario::all();
+
+        return view('livewire.crear-vacante', ['salarios' => $salarios]);
     }
 }
--- resources/views/livewire/crear-vacante.blade.php
+++ resources/views/livewire/crear-vacante.blade.php
@@ -11,6 +11,10 @@
         <x-label for="salario" :value="__('Salario Mensual')" />
         <select name="salario" id="salario"
             class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full">
+            <option value="">-- Seleccione --</option>
+            @foreach ($salarios as $salario)
+                <option value="{{ $salario->id }}">{{ $salario->salario }}</option>
+            @endforeach
         </select>
     </div>
 
