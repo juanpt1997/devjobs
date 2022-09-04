2f73b05af56dbed35418e791b4e2ae18b7f4e38e
juan.tabares <juan.tabares@coytex.com.co>
2022-09-04 12:52:54 -0500

    agregado policy para que otros usuarios no puedan editar una vacante que no fue creada por ellos mismos

--- app/Http/Controllers/VacanteController.php
+++ app/Http/Controllers/VacanteController.php
@@ -57,6 +57,8 @@ class VacanteController extends Controller
      */
     public function edit(Vacante $vacante)
     {
+        $this->authorize('update', $vacante);
+
         return view('vacantes.edit', ['vacante' => $vacante]);
     }
 
