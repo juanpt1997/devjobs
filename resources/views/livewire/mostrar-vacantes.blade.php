a6dfc7a151aa27d2e5828462ef3e3c9a9bf47f5b
juan.tabares <juan.tabares@coytex.com.co>
2022-09-04 11:26:26 -0500

    implementado forelse para mostrar mensaje cuando no hay vacantes

--- resources/views/livewire/mostrar-vacantes.blade.php
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -1,6 +1,6 @@
 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
 
-    @foreach ($vacantes as $vacante)
+    @forelse ($vacantes as $vacante)
         <div class="p-6 bg-white border-b border-gray-200 md:flex md:justify-between md:items-center">
             <div class="space-y-3">
                 <a href="#" class="text-xl font-bold">
@@ -11,17 +11,21 @@
             </div>
 
             <div class="gap-3 flex flex-col items-stretch mt-5 md:flex-row md:mt-0">
-                <a href="#" class="bg-slate-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
+                <a href="#"
+                    class="bg-slate-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
                     Candidatos
                 </a>
-                <a href="#" class="bg-blue-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
+                <a href="#"
+                    class="bg-blue-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
                     Editar
                 </a>
-                <a href="#" class="bg-red-600 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
+                <a href="#"
+                    class="bg-red-600 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
                     Eliminar
                 </a>
             </div>
         </div>
-
-    @endforeach
+    @empty
+        <p class="p-3 text-center text-sm text-gray-600">No hay vacantes que mostrar</p>
+    @endforelse
 </div>
