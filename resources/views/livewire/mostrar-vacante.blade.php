0d27be6a684585cf47047c64866378844c68710c
juan.tabares <juan.tabares@coytex.com.co>
2022-09-11 12:44:21 -0500

    mostrando descripcion del puesto y la imagen

--- resources/views/livewire/mostrar-vacante.blade.php
+++ resources/views/livewire/mostrar-vacante.blade.php
@@ -24,4 +24,14 @@
             </p>
         </div>
     </div>
+
+    <div class="md:grid md:grid-cols-6 gap-4">
+        <div class="md:col-span-2">
+            <img src="{{ asset('storage/vacantes/' . $vacante->imagen) }}" alt="{{ 'Imagen vacante ' . $vacante->titulo }}">
+        </div>
+        <div class="md:col-span-4">
+            <h2 class="text-2xl font-bold mb-5">Descripción del puesto</h2>
+            <p>{{ $vacante->descripcion }}</p>
+        </div>
+    </div>
 </div>
