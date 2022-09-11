5a2be273641227cd4f77834050c21e384f560205
juan.tabares <juan.tabares@coytex.com.co>
2022-09-11 13:09:21 -0500

    modificado policy para que el usuario developer no pueda acceder a ver sus vacantes creadas, ni crear vacantes, puesto que esto solo es permitido para los reclutadores

--- resources/views/livewire/mostrar-vacante.blade.php
+++ resources/views/livewire/mostrar-vacante.blade.php
@@ -34,4 +34,12 @@
             <p>{{ $vacante->descripcion }}</p>
         </div>
     </div>
+
+    @guest
+        <div class="mt-5 bg-gray-50 border border-dashed p-5 text-center">
+            <p>
+                ¿Desear aplicar a esta vacante? <a class="font-bold text-indigo-600" href="{{ route('register') }}">Obten una cuenta y aplica a esta y otras vacantes</a>
+            </p>
+        </div>
+    @endguest
 </div>
