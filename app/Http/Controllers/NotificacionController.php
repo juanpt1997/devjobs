cfb712e937d0d1251bef820c04cb478df0d94680
juan.tabares <juan.tabares@coytex.com.co>
2022-09-18 16:27:36 -0500

    limpiando las notificaciones

--- app/Http/Controllers/NotificacionController.php
+++ app/Http/Controllers/NotificacionController.php
@@ -15,6 +15,10 @@ class NotificacionController extends Controller
     public function __invoke(Request $request)
     {
         $notificaciones = auth()->user()->unreadNotifications;
+
+        // Limpiar notificaciones
+        auth()->user()->unreadNotifications->markAsRead();
+
         return view('notificaciones.index', [
             'notificaciones' => $notificaciones
         ]);
