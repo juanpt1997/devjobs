26259de5a47e0f426228039d74dbf05bf6b0b5b0
juan.tabares <juan.tabares@coytex.com.co>
2022-09-19 21:39:17 -0500

    creando la vista principal

---
+++ app/Http/Livewire/HomeVacantes.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class HomeVacantes extends Component
+{
+    public function render()
+    {
+        return view('livewire.home-vacantes');
+    }
+}
---
+++ resources/views/livewire/home-vacantes.blade.php
@@ -0,0 +1,5 @@
+<div>
+    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+
+    <p>vacantes..</p>    
+</div>
