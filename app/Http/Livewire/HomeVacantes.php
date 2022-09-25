1de966dd3b0cc0b2518c542b273ade61951ff951
juan.tabares <juan.tabares@coytex.com.co>
2022-09-25 15:22:47 -0500

    mostrando todas las vacantes disponibles en la pagina principal

--- app/Http/Livewire/HomeVacantes.php
+++ app/Http/Livewire/HomeVacantes.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Vacante;
 use Livewire\Component;
 
 class HomeVacantes extends Component
 {
     public function render()
     {
-        return view('livewire.home-vacantes');
+        $vacantes = Vacante::all();
+
+        return view('livewire.home-vacantes', compact('vacantes'));
     }
 }
